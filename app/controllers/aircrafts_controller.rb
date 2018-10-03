aeb649f5ae230d4a57d5929560aaf9770f650660
MaiHal <myturn.lovenyc@gmail.com>
2018-10-03 23:46:46 +0900

    aircraftsのshowページ大枠を作成

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -7,5 +7,7 @@ class AircraftsController < ApplicationController
 
 	def show
 		@aircraft = Aircraft.find_by(id: params[:id])
+		@airline = Airline.find_by(id: @aircraft.airline_id)
+		@airport = Airport.find_by(id: @aircraft.airport_id)
 	end
 end
