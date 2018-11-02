1f44a392952f16f0831153f25c75b77429f01830
MaiHal <myturn.lovenyc@gmail.com>
2018-11-02 14:23:08 +0900

    フラッシュメッセージを表示する処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,14 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(users_params)
 		if @user.save
+			session[:user_id] = @user.id
+			flash[:notice] = "会員登録完了"
 			redirect_to("/")
 		else
 			reder("signup")
 		end
 	end
-	
+
 	def users_params
 		params.require(:user).permit(:name, :email, :password)
 	end
