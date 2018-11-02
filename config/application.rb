72f85c261081d264f9265273adef880243632587
MaiHal <myturn.lovenyc@gmail.com>
2018-11-02 18:10:46 +0900

    戻した

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Flyapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = flse
     config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
