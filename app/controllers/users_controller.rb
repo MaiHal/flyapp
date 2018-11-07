2947b1179bf424596539bc0a760a549abc5d7dce
MaiHal <myturn.lovenyc@gmail.com>
2018-11-07 14:53:28 +0900

    新規登録後のリダイレクト先を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 		if @user.save
 			session[:user_id] = @user.id
 			flash[:notice] = "会員登録完了"
-			redirect_to("/")
+			redirect_to("/user/#{session[:user_id]}")
 		else
 			render("users/new")
 		end
